7009b216c5fd556c34b20041e59b28b191cd2900
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-08-20 09:13:23 +0200

    Added blog to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,43 @@
-import * as React from "react"
-import Coby from "../images/Cobydesigns2.png"
-import CodingSaturn from "../images/Coding-Saturn3.jpeg"
-import GlitteringFoxReads from "../images/Glitteringfoxreads2.png"
+import * as React from 'react'
+import Blog from '../images/Blog-Cleaner2.png'
+import CodingSaturn from '../images/Coding-Saturn3.jpeg'
+import GlitteringFoxReads from '../images/Glitteringfoxreads2.png'
 
 function Projects(props) {
-  return (
-    <section className={props.hide ? 'projects show-projects' : 'projects'} ref={props.reference} id="Projects">
-        <h2>Projects</h2>
-        <div className="projects-container">
-            <div className="projects-group">
+    return (
+        <section className={props.hide ? 'projects show-projects' : 'projects'} ref={props.reference} id='Projects'>
+            <h2>Projects</h2>
+            <div className='projects-container'>
+                <div className='projects-group'>
 
-                    <a href="http://cobydesigns.budgetchamp.net/" target="_blank" rel="noopener noreferrer">
-                        <img alt="Project1" src={Coby}/>
+                    <a href='http://blog.infinidream.net/' target='_blank' rel='noopener noreferrer'>
+                        <img alt='Project1' src={Blog}/>
                         <ul> 
-                            <h4> Cobydesigns </h4>
-                            <p>My old portfoloio </p>
-                            <p className="project-tags"> Tags: WordPress, Bootstrap </p>
+                            <h4> Blog infinidream </h4>
+                            <p>My current blog </p>
+                            <p className='project-tags'> Tags: React, Gatsby, Node.js, Express.js </p>
                         </ul>
                     </a>
-                    <a href="https://glitteringfoxreads.com/" target="_blank" rel="noopener noreferrer">
-                        <img alt="Project2" src={GlitteringFoxReads}/>
+                    <a href='https://glitteringfoxreads.com/' target='_blank' rel='noopener noreferrer'>
+                        <img alt='Project2' src={GlitteringFoxReads}/>
                         <ul> 
                             <h4> GlitteringFoxReads </h4>
                             <p> A blogging site made for book reviewing indie books.</p>
-                            <p className="project-tags"> Tags: WordPress </p>
+                            <p className='project-tags'> Tags: WordPress </p>
                         </ul>
                     </a>
-                    <a href="https://bruce-hopkins-jr.github.io/Coding-Saturn/" target="_blank" rel="noopener noreferrer">
-                        <img alt="Project3" src={CodingSaturn}/>
+                    <a href='https://bruce-hopkins-jr.github.io/Coding-Saturn/' target='_blank' rel='noopener noreferrer'>
+                        <img alt='Project3' src={CodingSaturn}/>
                         <ul> 
                             <h4> Coding-Saturn </h4>
-                            <p> A template for a group of developers who host online classes. </p>
-                            <p className="project-tags"> Tags: React, Gatsby </p>
+                            <p> A template for an online course. </p>
+                            <p className='project-tags'> Tags: React, Gatsby </p>
                         </ul>
                     </a>
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Projects
