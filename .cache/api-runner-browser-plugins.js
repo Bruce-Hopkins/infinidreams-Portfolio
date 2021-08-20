62eb6c2c49dcec1f764d313669e56b6e6fafb80c
Bruce Hopkins Jr <behopkinsjr@gmail.com>
2021-08-20 07:42:55 +0200

    Delete .cache directory

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Infinidreams","short_name":"Infinidreams","start_url":"/","background_color":"#FFFFFF","theme_color":"006D77","display":"minimal-ui","icon":"src/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"c69d08068e9ed9edd8a36ef5b80576c1"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
-      options: {"plugins":[],"trackingIds":["UA-190645408-1 "]},
-    }]
